8f76b42a2222d596e90872d6d6832298d112a3d1
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2025-09-29 15:52:22 +0900

    プロジェクト ファイルを追加します。

---
+++ Object3d.PS.hlsl
@@ -0,0 +1,23 @@
+#include "object3d.hlsli"
+
+struct Material
+{
+    float32_t4 color;
+};
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+ConstantBuffer<Material> gMaterial : register(b0);
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    output.color = gMaterial.color * textureColor;
+    
+    return output;
+}
