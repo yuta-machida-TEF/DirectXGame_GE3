54d5cfade97cb576af0d4d7f743013e31b128714
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2025-12-22 12:12:38 +0900

    e

--- project/main.cpp
+++ project/main.cpp
@@ -364,29 +364,29 @@ void Log(const std::string& message)
 	OutputDebugStringA(message.c_str());
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
-	WPARAM wparam, LPARAM lparam) {
-
-	
-	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam))
-	{
-		return true;
-	}
-
-	//メッセージに応じてゲーム固有の処理を行う
-	switch (msg)
-	{
-	case WM_DESTROY:
-
-		//OSに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	//標準のメッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-
-}
+//LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
+//	WPARAM wparam, LPARAM lparam) {
+//
+//	
+//	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam))
+//	{
+//		return true;
+//	}
+//
+//	//メッセージに応じてゲーム固有の処理を行う
+//	switch (msg)
+//	{
+//	case WM_DESTROY:
+//
+//		//OSに対して、アプリの終了を伝える
+//		PostQuitMessage(0);
+//		return 0;
+//	}
+//
+//	//標準のメッセージ処理を行う
+//	return DefWindowProc(hwnd, msg, wparam, lparam);
+//
+//}
 
 //DepthStencilTextureを作る
 ID3D12Resource* CreateDepthStencilTextureResource(ID3D12Device* device, int32_t width, int32_t height)
