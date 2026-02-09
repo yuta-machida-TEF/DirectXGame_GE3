7c3cd6904c733ab188ee37d89129bd63d5e0b55b
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-02-09 18:53:42 +0900

    4章完了

--- project/DiretXCommon.cpp
+++ project/DiretXCommon.cpp
@@ -473,9 +473,9 @@ void DirectXCommon::InitializeFixFPS()
 void DirectXCommon::UpdateFixFPS()
 {
 	//1/60秒ぴったりの時間
-	const std::chrono::microseconds kMinTime(uint64_t(1000000.0f / 60.0f));
+	const std::chrono::microseconds kMinTime(uint64_t(1000000.0f / 10.0f));
    //1/60秒よりわずかに短い時間
-	const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f / 65.0f));
+	const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f / 15.0f));
 	//現在時間を取得する
 	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
 	//前回記録からの経過時間を取得する
